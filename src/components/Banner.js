0a459a930bb98343125e1241ef5c22affd3a92c8
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-23 22:26:17 +0900

    Banner Component 작성 및 index.html lang ko로 변경

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
 import requests from "../api/request";
+import "./Banner.css";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -21,7 +22,27 @@ const Banner = () => {
     setMovie(movieDetail);
   };
 
-  return <div>Banner</div>;
+  const truncate = (str, n) => {
+    return str?.length > n ? `${str.substring(0, n - 1)}...` : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
+        <div className="banner__buttons">{movie?.videos?.results[0]?.key && <button className="banner__button play">Play</button>}</div>
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
 };
 
 export default Banner;
