4755a5f778717ba77d10a09064b49e681ddc37bb
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-23 21:59:46 +0900

    App.css 수정 & Banner Component 작성 & request.js 오타수정

--- src/api/request.js
+++ src/api/request.js
@@ -1,12 +1,12 @@
-const request = {
+const requests = {
   fetchNowPlaying: "/movie/now_playing",
   fetchTrending: "/trending/all/week",
   fetchTopRated: "/movie/top_rated",
-  fetchActionMovies: "/discovery/movie?with_genres=28",
-  fetchComedyMovies: "/discovery/movie?with_genres=35",
-  fetchHorrorMovies: "/discovery/movie?with_genres=27",
-  fetchRomanceMovies: "/discovery/movie?with_genres=10749",
+  fetchActionMovies: "/discover/movie?with_genres=28",
+  fetchComedyMovies: "/discover/movie?with_genres=35",
+  fetchHorrorMovies: "/discover/movie?with_genres=27",
+  fetchRomanceMovies: "/discover/movie?with_genres=10749",
   fetchDocumentaries: "/discover/movie?with_genres=99",
 };
 
-export default request;
+export default requests;
