88789703298541e2ed9748fcf14b8ab8daa2a76d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-26 11:11:03 +0900

    Modal이 열려있을때 외부를 클릭 또는 터치 시 Modal이 닫히게 수정

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./MovieModal.css";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const MovieModal = ({ backdrop_path, title, overview, name, release_date, first_air_date, vote_average, setModalOpen }) => {
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
@@ -15,7 +22,7 @@ const MovieModal = ({ backdrop_path, title, overview, name, release_date, first_
               <span className="modal__user_perc">100% for you</span> {release_date ? release_date : first_air_date}
             </p>
             <h2 className="modal__title">{title ? title : name}</h2>
-            <p className="modal__overview">평정: {vote_average}</p>
+            <p className="modal__overview">평점: {vote_average}</p>
             <p className="modal__overview">{overview}</p>
           </div>
         </div>
